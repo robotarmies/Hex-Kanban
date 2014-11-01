2f046a28c009a9014ea4b6602a59798938263868
Jim Simon <jim@blueacorn.com>
2014-11-01 16:18:01 -0400

    Broke out html to separate blocks.  Implemented javascript to dynamically populate the top panel on home page

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -20,21 +20,34 @@ function importCurrentWork(json) {
             'ui_hours': obj.gsx$ui.$t
         });
     });
+
+    $('.gdx-num-tasks').html(work.length);
+    $('.gdx-num-completed').html(work.filter(function(task) {
+        return task.status === 'Completed';
+    }).length);
 }
 
 function importBacklog(json) {
     $(json.feed.entry).each(function(index, obj) {
-        backlog.push({
-            'client': obj.gsx$client.$t,
-            'deadline': obj.gsx$deadline.$t,
-            'dev_hours': obj.gsx$dev.$t,
-            'priority': obj.gsx$priority.$t,
-            'qa_hours': obj.gsx$qa.$t,
-            'taskname': obj.gsx$taskname.$t,
-            'attask_ref': obj.gsx$taskref.$t,
-            'ui_hours': obj.gsx$ui.$t
-        });
+        if (obj.gsx$taskname.$t !== '') {
+            backlog.push({
+                'client': obj.gsx$client.$t,
+                'deadline': obj.gsx$deadline.$t,
+                'dev_hours': obj.gsx$dev.$t,
+                'priority': obj.gsx$priority.$t,
+                'qa_hours': obj.gsx$qa.$t,
+                'taskname': obj.gsx$taskname.$t,
+                'attask_ref': obj.gsx$taskref.$t,
+                'ui_hours': obj.gsx$ui.$t,
+                'is_defect': obj.gsx$defect.$t
+            });
+        }
     });
+
+    $('.gdx-num-backlog').html(backlog.length);
+    $('.gdx-num-defects').html(backlog.filter(function(task) {
+        return task.is_defect === 'Yes';
+    }).length);
 }
 
 function importDataValidation(json) {
